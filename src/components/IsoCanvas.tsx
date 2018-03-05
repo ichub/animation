0f3624c6ca9e94283b949c220ab72e9daae6eba6
Ivan Chub <ivansergchub@gmail.com>
2018-03-05 14:12:37 -0800

    WIP

--- src/components/IsoCanvas.tsx
+++ src/components/IsoCanvas.tsx
@@ -3,6 +3,10 @@ import * as Radium from "radium";
 import * as gl from "gl-matrix";
 import {Transformation} from "../math/Transformation";
 
+declare class GIFEncoder {
+
+}
+
 @Radium
 export class IsoCanvas extends React.Component<any, IIsoCanvasState> {
     refs: {
@@ -11,35 +15,131 @@ export class IsoCanvas extends React.Component<any, IIsoCanvasState> {
 
     ctx: CanvasRenderingContext2D;
 
-    circlePos = gl.vec2.fromValues(100, 100);
-    circleVel = gl.vec2.fromValues(0, 0);
-    offsetX = 0;
-    currentWaitingTime = 100;
-    pausedVelocity = gl.vec2.fromValues(0, 0);
+    circlePos;
+    circleVel;
+    offsetX;
+    currentWaitingTime;
     strokeSize = 5;
 
     img: HTMLImageElement;
+    encoder: any;
 
+    framesToRecord = 60 * 5;
+    recordedFrames = 0;
+    permutations = [];
+    currentPermutation = 0;
+    jumping = true;
+    firstContact = false;
+    moving = true;
+    recording = false;
 
     constructor() {
         super();
 
+        this.resetState();
+
+        this.img = document.createElement("img");
+        this.img.src = "/background.png";
+
+        this.permutations = this.generatePermutations([]);
+        this.encoder = new GIFEncoder();
+        this.encoder.setRepeat(0);
+        this.encoder.setDelay(Math.floor(1000 / 60)); //go to next frame every n milliseconds
+    }
+
+    currentPermutationToName() {
+        let perm = this.permutations[this.currentPermutation - 1];
+        let result = "";
+
+        for (let item of perm) {
+            for (let name of Object.keys(item)) {
+                result += name + "_" + item[name].toString().replace(".", "-");
+                this.state[name] = item[name];
+            }
+        }
+
+        return result + ".gif";
+    }
+
+    loadNextPermutation() {
+        if (this.currentPermutation >= this.permutations.length) {
+            alert("done");
+            return;
+        }
+
+        let perm = this.permutations[++this.currentPermutation];
+
+        for (let item of perm) {
+            for (let name of Object.keys(item)) {
+                this.state[name] = item[name];
+            }
+        }
+
+        this.setState<any>(this.state);
+    }
+
+    resetState() {
         this.state = {
-            width: 800 * 0.75,
-            height: 600 * 0.75,
+            width: 300,
+            height: 300,
             speedX: 1,
-            speedY: 1,
+            speedY: 4,
             jumpWait: 100,
             circleRadius: 50,
             gravity: 1
         };
+        this.offsetX = 0;
+        this.circlePos = gl.vec2.fromValues(100, this.state.height);
+        this.circleVel = gl.vec2.fromValues(0, 0);
+        this.currentWaitingTime = 99;
+        this.firstContact = true;
+    }
 
-        this.img = document.createElement("img");
-        this.img.src = "/background.png"
+    generatePermutations(used) {
+        let potential = {
+            "speedX": [1, 20],
+            "speedY": [4, 1],
+            "gravity": [0.3],
+            "jumpWait": [50, 500],
+            "circleRadius": [50]
+        };
+
+        let perm = [];
+
+        // choose the next non fixed item
+        let current = null;
+        for (let name of Object.keys(potential)) {
+            if (used.indexOf(name) == -1) {
+                current = name;
+                break;
+            }
+        }
+
+        if (current == null) {
+            return [[]];
+        }
+
+        let options = potential[current];
+
+        used.push(current);
+        let next = this.generatePermutations(used);
+        used.pop(current);
+
+        let perms = [];
+
+        for (let tail of next) {
+            for (let option of options) {
+                perms.push([{[current]: option}, ...tail]);
+            }
+        }
+
+        return perms;
     }
 
     componentDidMount() {
         this.ctx = this.refs.canvas.getContext("2d");
+        this.resetState();
+        this.loadNextPermutation();
         this.start();
     }
 
@@ -62,27 +162,54 @@ export class IsoCanvas extends React.Component<any, IIsoCanvasState> {
         this.ctx.arc(this.circlePos[0], this.circlePos[1], this.state.circleRadius, 0, 2 * Math.PI);
         this.ctx.stroke();
         this.ctx.fill();
+
+        if (this.recording) {
+            if (this.recordedFrames == 0) {
+                this.encoder.start();
+            }
+
+            if (this.recordedFrames++ < this.framesToRecord) {
+                this.encoder.addFrame(this.ctx);
+                console.log("recorded frame: " + this.recordedFrames);
+            } else {
+
+                this.encoder.finish();
+                this.encoder.download(this.currentPermutationToName());
+                this.resetState();
+                this.loadNextPermutation();
+                this.recordedFrames = 0;
+            }
+        }
     }
 
     update() {
-        if (this.currentWaitingTime < this.state.jumpWait) {
+        gl.vec2.add(this.circlePos, this.circlePos, this.circleVel);
+        gl.vec2.add(this.circleVel, this.circleVel, gl.vec2.fromValues(0, this.state.gravity / 10 * this.state.speedY));
+
+
+        if (this.circlePos[1] + this.state.circleRadius + this.strokeSize / 2 > this.state.height) {
+            this.jumping = false;
+            this.currentWaitingTime = 0;
+            this.circleVel[1] = 0;
+            this.circlePos[1] = this.state.height - this.state.circleRadius - this.strokeSize / 2;
+        }
+
+        if (this.currentWaitingTime < this.state.jumpWait && !this.jumping) {
             this.currentWaitingTime++;
         } else {
-            gl.vec2.add(this.circlePos, this.circlePos, this.circleVel);
-            gl.vec2.add(this.circleVel, this.circleVel, gl.vec2.fromValues(0, this.state.gravity / 10 * this.state.speedY));
+            this.jumping = true;
+            this.currentWaitingTime = 0;
+            this.circleVel[1] = -this.state.speedY;
 
-            if (this.circlePos[1] + this.state.circleRadius + this.strokeSize / 2 > this.state.height) {
-                this.circleVel[1] = -this.state.speedY;
-                this.circlePos[1] = this.state.height - this.state.circleRadius - this.strokeSize / 2;
-                this.currentWaitingTime = 0;
-            }
+        }
 
-            if (this.offsetX < -this.img.naturalWidth) {
-                this.offsetX = 0;
-            } else if (this.offsetX > this.img.naturalWidth) {
-                this.offsetX = 0;
-            }
+        if (this.offsetX < -this.img.naturalWidth) {
+            this.offsetX = 0;
+        } else if (this.offsetX > this.img.naturalWidth) {
+            this.offsetX = 0;
+        }
 
+        if (this.moving) {
             this.offsetX -= this.state.speedX;
         }
     }
@@ -178,7 +305,8 @@ export class IsoCanvas extends React.Component<any, IIsoCanvasState> {
 
 const style = {
     canvas: {
-        border: "1px solid black"
+        border: "1px solid black",
+        marginBottom: 150
     },
     input: {
         margin: "10px"
